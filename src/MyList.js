dc58ba351dd5f2c2739f99fc8823ee3d97318b9b
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-26 18:45:14 -0400

    [newItem] newItem function

--- src/MyList.js
+++ src/MyList.js
@@ -3,17 +3,61 @@ import './App.css'
 import ListItem from './ListItem'
 
 class MyList extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      toDoItemArray: props.theList,
+      newItem: ""
+    }
+  }
+
+  clearList (e) {
+    console.log("Clearing List")
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
+  newItemChange = (e) => {
+    this.setState({
+      newItem: e.target.value
+    })
+
+  }
+
+  addItem = (e) => {
+    e.preventDefault()
+     // when form is submitted
+    //make a copy of toDoItemArray
+    let thisArray = this.state.toDoItemArray
+    thisArray.push(this.state.newItem)
+    this.setState({
+      newItem: '',
+      toDoItemArray: thisArray
+    })
+  }
+
   render() {
-    let todoItems = this.props.theList.map((item, index) => (
-      <ListItem doThis={item} key={'todo' + index} />
+
+    let todoItems = this.state.toDoItemArray.map( (item, index) => (
+      <ListItem doThis={item} key={index} />
     ))
-    
+
     return (
       <div>
-        <h1>Things I should stop procrastinating:</h1>
-        <ul>
-          {todoItems}
-        </ul>
+      <h1>Things I should stop procrastinating:</h1>
+      <ul>
+        {todoItems}
+      </ul>
+      <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
+      <form>
+      <input type="text"
+      placeholder="Type an item here"
+      onChange={(e) => this.newItemChange(e)}
+      value={this.state.newItem}
+      />
+      <button onClick={(e) => this.addItem(e)}>Add it!</button>
+      </form>
       </div>
     )
   }
